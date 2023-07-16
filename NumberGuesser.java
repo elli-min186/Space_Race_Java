3443f866ca61056988de7b4d1b54701959c5a70a
Elli <ellishun03@gmail.com>
2023-07-16 16:50:33 -0400

    Add new files

--- NumberGuesser.java
+++
@@ -1,8 +0,0 @@
-package Number_guess_java;
-public class NumberGuesser{
-  public static void main(String arg[]){
-
-    System.out.println("Sup");
-
-  }
-}
---
+++ SpaceRace.java
@@ -0,0 +1,7 @@
+public class SpaceRace {
+    public static void main(String arg[]){
+  
+      System.out.println("Hello World");
+  
+    }
+}
