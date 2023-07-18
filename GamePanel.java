10ec5c7947cac28b0fd07833e4dfb06320f10ae7
Elli <ellishun03@gmail.com>
2023-07-18 19:11:47 -0400

    Resize the screen and set screen

--- GamePanel.java
+++ GamePanel.java
@@ -1,8 +1,25 @@
+import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable {
-    public GamePanel() {
-        ;
+    static final int SCREENWIDTH = 1280;
+    static final int SCREENHEIGHT = 720;
+    Thread game; //separate from all the tasks
+    Graphics graphics;
+    static final Dimension SCREEN = new Dimension(SCREENWIDTH, SCREENHEIGHT);
+    public enum modes { //create a new class to show specfic screen
+        TITLE,
+        MODE,
+        GAME,
+        OVER
+    }
+    private modes start;
+    public GamePanel() { //constructor; always run first
+        this.setFocusable(true);
+        this.setPreferredSize(SCREEN);
+        this.setMaximumSize(SCREEN);
+        this.setMinimumSize(SCREEN);
     }
     public void run() {
         ;
