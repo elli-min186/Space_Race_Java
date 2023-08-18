3ea8b2c5e0a480fa2fdb9dc5f3f24c6b694d3c17
MoNakouzi <mo.nakouzi03@gmail.com>
2023-08-18 17:01:25 -0400

    rand.nextint bound fixing

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -18,13 +18,11 @@ public class GamePanel extends JPanel implements Runnable {
     static final int TIMEWIDTH = 20;
     static int counter = 0;
 
-
-    static final int P1RECTX1 = (((SCREENWIDTH/2 + 10) + SCREENWIDTH)/2) - (SPACESHIPWIDTH/2);
+    static final int P1RECTX1 = (((SCREENWIDTH / 2 + 10) + SCREENWIDTH) / 2) - (SPACESHIPWIDTH / 2);
     static final int P1RECTY1 = SCREENHEIGHT - SPACESHIPHEIGHT - 10;
 
-    static final int P2RECTX1 = ((SCREENWIDTH/2)-10)/2 - (SPACESHIPWIDTH/2);
+    static final int P2RECTX1 = ((SCREENWIDTH / 2) - 10) / 2 - (SPACESHIPWIDTH / 2);
     static final int P2RECTY1 = SCREENHEIGHT - SPACESHIPHEIGHT - 10;
-    
 
     Thread gameThread; // separate from all the tasks
     Graphics graphics;
@@ -48,9 +46,8 @@ public class GamePanel extends JPanel implements Runnable {
     public Spaceship spaceship2;
     public ArrayList<Balls> ballsArraylist;
 
-
     public GamePanel() { // constructor; always run first
-        
+
         this.setFocusable(true);
         this.setPreferredSize(SCREEN);
         this.setMaximumSize(SCREEN);
@@ -63,22 +60,18 @@ public class GamePanel extends JPanel implements Runnable {
         this.endScreen = new EndScreen(0, 0, SCREENWIDTH, SCREENHEIGHT);
         this.time = new Time(SCREENWIDTH / 2 - 10, 2, TIMEWIDTH, SCREENHEIGHT - 5);
 
-        
         this.spaceship1 = new Spaceship(P1RECTX1, P1RECTY1, SPACESHIPWIDTH, SPACESHIPHEIGHT, 1);
         this.spaceship2 = new Spaceship(P2RECTX1, P2RECTY1, SPACESHIPWIDTH, SPACESHIPHEIGHT, 2);
 
         ballsArraylist = new ArrayList<>();
         for (int i = 0; i < BALLCOUNT; i++) { // all 30 balls in arraylist
-            if (i < BALLCOUNT/2) {
+            if (i < BALLCOUNT / 2) {
                 ballsArraylist.add(newBallLeft());
-            }
-            else {
+            } else {
                 ballsArraylist.add(newBallRight());
             }
 
         }
-
-  
 
         this.states = modes.GAME; // initialize states(enum)
         this.gameThread = new Thread(this); // thread running game panel
@@ -88,16 +81,16 @@ public class GamePanel extends JPanel implements Runnable {
 
     public Balls newBallLeft() { // a new ball at random y from the left
         Random rand = new Random();
-        int randomY = rand.nextInt(BALLRADIUS*2, SCREENHEIGHT-90);
-        int randomX = rand.nextInt(SCREENWIDTH * 1/2);
-        return new Balls(randomX,randomY,BALLRADIUS * 2,BALLRADIUS * 2);
+        int randomY = rand.nextInt(SCREENHEIGHT - 90 - BALLRADIUS * 2) + BALLRADIUS * 2;
+        int randomX = rand.nextInt(SCREENWIDTH * 1 / 2);
+        return new Balls(randomX, randomY, BALLRADIUS * 2, BALLRADIUS * 2);
     }
 
     public Balls newBallRight() { // a new ball at random y from the right
         Random rand = new Random();
-        int randomY = rand.nextInt(BALLRADIUS*2, SCREENHEIGHT-90);
-        int randomX = rand.nextInt(SCREENWIDTH * 1/2, SCREENWIDTH);
-        return new Balls(randomX,randomY,BALLRADIUS * 2,BALLRADIUS * 2);
+        int randomY = rand.nextInt(SCREENHEIGHT - 90 - BALLRADIUS * 2) + BALLRADIUS * 2;
+        int randomX = rand.nextInt(SCREENWIDTH - SCREENWIDTH * 1 / 2) + SCREENWIDTH * 1 / 2;
+        return new Balls(randomX, randomY, BALLRADIUS * 2, BALLRADIUS * 2);
     }
 
     @SuppressWarnings("InfiniteLoopStatement")
@@ -125,13 +118,12 @@ public class GamePanel extends JPanel implements Runnable {
         spaceship2.moveX();
         spaceship2.moveY();
         for (int i = 0; i < BALLCOUNT; i++) {
-            if (i < BALLCOUNT/2) { // first half of the balls move left
+            if (i < BALLCOUNT / 2) { // first half of the balls move left
                 ballsArraylist.get(i).moveLeft();
+            } else { // other half move right
+                ballsArraylist.get(i).moveRight();
             }
-            else { // other half move right
-                ballsArraylist.get(i).moveRight(); 
-            }
-            
+
         }
         if (time.y < SCREENHEIGHT) {
             if (counter % 6 == 0) { // time run outs in approximately 1 min
@@ -147,13 +139,12 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void checkBallCollision() {
-        for (int i = 0; i < BALLCOUNT; i++) { 
+        for (int i = 0; i < BALLCOUNT; i++) {
             Balls currentBall = ballsArraylist.get(i);
             if (currentBall.x >= SCREENWIDTH) {
                 ballsArraylist.remove(i);
                 ballsArraylist.add(i, newBallLeft());
-            }
-            else if (currentBall.x <= 0) {
+            } else if (currentBall.x <= 0) {
                 ballsArraylist.remove(i);
                 ballsArraylist.add(i, newBallRight());
             }
@@ -164,44 +155,47 @@ public class GamePanel extends JPanel implements Runnable {
 
         // spaceship 1
         // bottom part of spaceship and time frame top
-        if (((spaceship1.x >= time.getX() && spaceship1.x < time.getX() + TIMEWIDTH) || (spaceship1.x + SPACESHIPWIDTH >= time.getX() && spaceship1.x < time.getX())) && spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y) {
+        if (((spaceship1.x >= time.getX() && spaceship1.x < time.getX() + TIMEWIDTH)
+                || (spaceship1.x + SPACESHIPWIDTH >= time.getX() && spaceship1.x < time.getX()))
+                && spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y) {
             spaceship1.y = time.y - SPACESHIPHEIGHT;
         }
         // right wing of spaceship and left side of time frame
-        if (spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() >= time.getX() && spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() < time.getX() + TIMEWIDTH) {
-            if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y)
-            {
+        if (spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() >= time.getX()
+                && spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() < time.getX() + TIMEWIDTH) {
+            if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y) {
                 spaceship1.x = time.x - spaceship1.getWingWidth() - spaceship1.width;
             }
-        } 
+        }
         // left wing of spaceship and the right side of time frame
-        if (spaceship1.x - spaceship1.getWingWidth() <= time.getX() + TIMEWIDTH && spaceship1.x - spaceship1.getWingWidth() > time.getX()) {
-            if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y)
-            {
+        if (spaceship1.x - spaceship1.getWingWidth() <= time.getX() + TIMEWIDTH
+                && spaceship1.x - spaceship1.getWingWidth() > time.getX()) {
+            if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= time.y) {
                 spaceship1.x = time.x + TIMEWIDTH + spaceship1.getWingWidth();
             }
         }
 
-
         // spaceship 2
         // bottom part of spaceship and time frame top
-        if (((spaceship2.x >= time.getX() && spaceship2.x < time.getX() + TIMEWIDTH) || (spaceship2.x + SPACESHIPWIDTH >= time.getX() && spaceship2.x < time.getX())) && spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y) {
+        if (((spaceship2.x >= time.getX() && spaceship2.x < time.getX() + TIMEWIDTH)
+                || (spaceship2.x + SPACESHIPWIDTH >= time.getX() && spaceship2.x < time.getX()))
+                && spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y) {
             spaceship2.y = time.y - SPACESHIPHEIGHT;
         }
         // right wing of spaceship and left side of time frame
-        if (spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() >= time.getX() && spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() < time.getX() + TIMEWIDTH) {
-            if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y)
-            {
+        if (spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() >= time.getX()
+                && spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() < time.getX() + TIMEWIDTH) {
+            if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y) {
                 spaceship2.x = time.x - spaceship2.getWingWidth() - spaceship2.width;
             }
-        } 
+        }
         // left wing of spaceship and the right side of time frame
-        if (spaceship2.x - spaceship2.getWingWidth() <= time.getX() + TIMEWIDTH && spaceship2.x - spaceship2.getWingWidth() > time.getX()) {
-            if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y)
-            {
+        if (spaceship2.x - spaceship2.getWingWidth() <= time.getX() + TIMEWIDTH
+                && spaceship2.x - spaceship2.getWingWidth() > time.getX()) {
+            if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= time.y) {
                 spaceship2.x = time.x + TIMEWIDTH + spaceship2.getWingWidth();
             }
-        }        
+        }
     }
 
     public void checkBoundaryCollision() {
@@ -210,14 +204,13 @@ public class GamePanel extends JPanel implements Runnable {
         if (spaceship1.y - spaceship1.getHeadHeight() <= 0) { // if the spaceship goes beyond the top boundary
             spaceship1.y = P1RECTY1; // set y to original position
             spaceship1.x = P1RECTX1; // set x to original position
-        }
-        else if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= SCREENHEIGHT) { // bottom boundary
-            spaceship1.y = SCREENHEIGHT - SPACESHIPHEIGHT - spaceship1.getWingHeight(); // set the y as 0 so it doesnt go beyond it
+        } else if (spaceship1.y + SPACESHIPHEIGHT + spaceship1.getWingHeight() >= SCREENHEIGHT) { // bottom boundary
+            spaceship1.y = SCREENHEIGHT - SPACESHIPHEIGHT - spaceship1.getWingHeight(); // set the y as 0 so it doesnt
+                                                                                        // go beyond it
         }
         if (spaceship1.x - spaceship1.getWingWidth() <= 0) { // left boundary
             spaceship1.x = spaceship1.getWingWidth();
-        }
-        else if (spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() >= SCREENWIDTH) { // right boundary
+        } else if (spaceship1.x + SPACESHIPWIDTH + spaceship1.getWingWidth() >= SCREENWIDTH) { // right boundary
             spaceship1.x = SCREENWIDTH - SPACESHIPWIDTH - spaceship1.getWingWidth();
         }
 
@@ -225,14 +218,13 @@ public class GamePanel extends JPanel implements Runnable {
         if (spaceship2.y - spaceship2.getHeadHeight() <= 0) { // if the spaceship goes beyond the top boundary
             spaceship2.y = P2RECTY1; // set y to original position
             spaceship2.x = P2RECTX1; // set x to original position
-        }
-        else if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= SCREENHEIGHT) { // bottom boundary
-            spaceship2.y = SCREENHEIGHT - SPACESHIPHEIGHT - spaceship2.getWingHeight(); // set the y as 0 so it doesnt go beyond it
+        } else if (spaceship2.y + SPACESHIPHEIGHT + spaceship2.getWingHeight() >= SCREENHEIGHT) { // bottom boundary
+            spaceship2.y = SCREENHEIGHT - SPACESHIPHEIGHT - spaceship2.getWingHeight(); // set the y as 0 so it doesnt
+                                                                                        // go beyond it
         }
         if (spaceship2.x - spaceship2.getWingWidth() <= 0) { // left boundary
             spaceship2.x = spaceship2.getWingWidth();
-        }
-        else if (spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() >= SCREENWIDTH) { // right boundary
+        } else if (spaceship2.x + SPACESHIPWIDTH + spaceship2.getWingWidth() >= SCREENWIDTH) { // right boundary
             spaceship2.x = SCREENWIDTH - SPACESHIPWIDTH - spaceship2.getWingWidth();
         }
     }
@@ -291,6 +283,7 @@ public class GamePanel extends JPanel implements Runnable {
             spaceship1.keyPressed(e);
             spaceship2.keyPressed(e);
         }
+
         public void keyReleased(KeyEvent e) {
             spaceship1.keyReleased(e);
             spaceship2.keyReleased(e);
